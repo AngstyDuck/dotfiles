24713ae1d1d94c380ad3d5266349e0f15b426ff1
AngstyDuck <solsticedante@gmail.com>
2021-02-23 01:03:22 +0800

    yes

--- install.sh
+++ install.sh
@@ -12,21 +12,29 @@ sudo -u $USER pip3 install virtualenv
 
 # install vim plugins
 mkdir -p ~/.vim/pack/vendor/start
-
 git clone git://github.com/ajh17/VimCompletesMe.git ~/.vim/pack/vendor/start/VimCompletesMe
-
 git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
 vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
-
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 source ~/.bashrc
-
 git clone https://github.com/mileszs/ack.vim.git ~/.vim/pack/vendor/start/ack.vim
-
 apt-get install -y ripgrep
+git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.vim/pack/plugins/start/vim-tmux-navigator
+
+# install tmux plugins
+mkdir ~/.tmux_plugins
+# yanking was buggy so i removed it for now
+# git clone https://github.com/tmux-plugins/tmux-yank ~/.tmux_plugins/tmux-yank
+# git clone https://github.com/tmux-plugins/tmux-copycat ~/.tmux_plugins/tmux-copycat
+git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux_plugins/tmux-resurrect
+git clone https://github.com/tmux-plugins/tmux-continuum ~/.tmux_plugins/tmux-continuum
+git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.tmux_plugins/vim-tmux-navigator
+# install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 cp $DIR/vimrc ~/.vimrc
+cp $DIR/tmux.conf ~/.tmux.conf
 cp $DIR/gitconfig ~/.gitconfig
 
 # ssh key gen
