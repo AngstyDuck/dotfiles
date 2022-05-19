1b9ff440ec754b53bc57973e7024b638171b2b52
AngstyDuck <solsticedante@gmail.com>
2022-05-19 10:52:59 +0800

    yes

--- startup_run_this.sh
+++ startup_run_this.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-# setting caps lock button to control_L instead
-/usr/bin/setxkbmap -option "ctrl:nocaps"
+source ~/.bashrc
+sudo docker container start search-v4-sg-temp
+sudo docker container start search-v4-vn-temp
+sudo docker container start redis
+sudo docker container start mongo
+
+sudo ip link set enp6s0 mtu 1000
+sudo ip link set wlp3s0 mtu 1000
+
+# start vpn
+nmcli con up id '99 VPN'
+
+# restart nginx
+sudo service nginx stop
+sudo service nginx start
